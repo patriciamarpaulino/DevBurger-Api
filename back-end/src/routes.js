c3e29bb1f31661bf613a128c0a053cbeccc63d10
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-07-16 16:35:59 -0300

    configuração do model de usuário

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+import User from "./app/models/user"
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-    return response.status(200).json({ message: "Olá Mundo!" })
+routes.get("/", async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: "Raphael",
+        email: "raphael@email.com",
+        password_hash: "1gFv5yneef6DD3d",
+    })
+
+    return response.status(201).json(user)
 })
 
 module.exports = routes
