4272e61efc21c6aaaa622a229c1d10fbb11327cb
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-07-17 17:46:00 -0300

    criando controller de usuário

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,18 +1,8 @@
 import { Router } from "express"
-import { v4 } from "uuid"
-import User from "./app/models/user"
+import UserController from "./app/controllers/usercontroller"
 
 const routes = new Router()
 
-routes.get("/", async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: "Raphael",
-        email: "raphael@email.com",
-        password_hash: "1gFv5yneef6DD3d",
-    })
+routes.post("/users", UserController.store)
 
-    return response.status(201).json(user)
-})
-
-module.exports = routes
+export default routes
