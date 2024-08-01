7c9d943fd6b2181ff8e2053ac9364ee17113f22d
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-08-01 20:32:43 -0300

    configurando token jwt pt1

--- back-end/src/app/controllers/SessionController.js
+++ back-end/src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup"
-import User from "../models/user";
+import User from "../models/user"
+import jwt from "jsonwebtoken"
+import authConfig from "../../config/auth"
 
 class SessionController {
     async store(request, response) {
@@ -41,6 +43,9 @@ class SessionController {
             name: user.name, 
             email, 
             admin: user.admin,
+            token: jwt.sign({ id: user.id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            }),
         });
     }
 }
