9fc4d12bf2d8f95606cc6ea566e4ee07a22ee4fa
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-09-05 14:50:14 -0300

    criando relacionamento entre tabelas

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -12,8 +12,12 @@ class Database {
     }
 
     init() {
-        this.connection = new Sequelize(configDatabase)
-        models.map(model => model.init(this.connection))
+        this.connection = new Sequelize(configDatabase);
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model => model.associate && model.associate(this.connection.models),
+            );
     }
 }
 
