c608c05aa6e64d285647f488bfa4d1a52b84cb39
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-09-12 13:41:11 -0300

    criando conexão com o MongoDB

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize"
+import mongoose from "mongoose"
 import configDatabase from "../config/database"
 import User from "../app/models/user"
 import Product from "../app/models/product"
@@ -8,7 +9,8 @@ const models = [User, Product, Category]
 
 class Database {
     constructor() {
-        this.init()
+        this.init();
+        this.mongo();
     }
 
     init() {
@@ -18,6 +20,12 @@ class Database {
             .map(
                 model => model.associate && model.associate(this.connection.models),
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            "mongodb://localhost:27017/devburger"
+        );
     }
 }
 
