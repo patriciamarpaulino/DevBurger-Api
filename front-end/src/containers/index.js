5225ea46527194bb919b5610199560a641fc1ff0
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-14 14:29:42 -0300

    att

--- front-end/src/containers/index.js
+++
@@ -1,9 +0,0 @@
-export * from './Cart'
-export * from './Home'
-export * from './Login'
-export * from './Menu'
-export * from './Register'
-export * from './Checkout'
-export * from './CompletePayment'
-export * from './Admin'
-
--- front-end/src/utils/formatPrice.js
+++
@@ -1,7 +0,0 @@
-export const formatPrice = (value) => {
-    return new Intl.NumberFormat('pr-BR',{
-        style: 'currency',
-        currency: 'BRL',
-    }).format(value / 100);
-        
-};
